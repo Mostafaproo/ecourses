ec01d0ab6f831790742d4e2adc054149669c9dd1
mostafazidan <mostafa.livecode@gmail.com>
2022-02-14 15:34:01 +0200

    add dist file

---
+++ dist/precache-manifest.388c622fefae67b74e9d5e6b0da4c429.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506dfc63ed3e5fc99a7e",
+    "url": "/css/app.e7533ffb.css"
+  },
+  {
+    "revision": "d619bd2c76459ebb6760",
+    "url": "/css/chunk-vendors.d1f3de35.css"
+  },
+  {
+    "revision": "82a47edaeec1ee6bbabe0284cbbf100d",
+    "url": "/img/slider1.82a47eda.jpeg"
+  },
+  {
+    "revision": "685ad0052c5b35ee7050eda1d9ea004d",
+    "url": "/img/slider2.685ad005.jpg"
+  },
+  {
+    "revision": "44822ceb9d2288bca8320a705be92296",
+    "url": "/img/slider3.44822ceb.jpg"
+  },
+  {
+    "revision": "d1bf307c1a4eb963356d4f76949a8623",
+    "url": "/img/slider4.d1bf307c.png"
+  },
+  {
+    "revision": "e95b81eb0cb195a2dfd704d37d8320ac",
+    "url": "/img/slider5.e95b81eb.jpg"
+  },
+  {
+    "revision": "5b82a54314f9d53256439dc66d1e61c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506dfc63ed3e5fc99a7e",
+    "url": "/js/app.3277bf0a.js"
+  },
+  {
+    "revision": "d619bd2c76459ebb6760",
+    "url": "/js/chunk-vendors.2d4b04ac.js"
+  },
+  {
+    "revision": "d1f4f4e6824b063043f2d15e3d0affad",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
